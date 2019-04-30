605db59968c7177938b5fffe5f59e591b05f76cf
Kwasi A. Kwakwa <kwasi.kwakwa@gmail.com>
2019-04-30 11:52:57 +0100

    Added a function to tracking tools for plotting final track distances

--- tracking_tools.py
+++ tracking_tools.py
@@ -239,21 +239,45 @@ def plot_msd_dist(tracks):
     plt.tight_layout()
     return fig
 
-def plot_msds(tracks):
+
+def plot_msds(tracks, time=0.015):
     '''
     Takes a standardised Pandas dataframe containing per-track information and
     plots each individual MSD
     '''
     drawlines = []
     tracksgroup = tracks.groupby("track_no")
+
     fig = plt.figure(figsize=(10, 6))
     ax1 = fig.add_subplot(1, 1, 1)
     for name, group in tracksgroup:
-        drawlines.append(np.arange(1, len(group["MSDs"].values)+1)*0.03)
+        drawlines.append(np.arange(1, len(group["MSDs"].values)+1)*time)
         drawlines.append(group["MSDs"].values * 1e-6)
     ax1.loglog(*drawlines, color='0.2', linewidth=0.5, alpha=0.1)
     ax1.set_xlabel('time(s)')
     ax1.set_ylabel('Mean Squared Displacement($\mu$m$^{2}$)')
     ax1.set_title('labelled MWT')
+    fig.tight_layout()
+    return fig
+
+
+def plot_final_positions(tracks, time=0.015):
+    '''
+    Takes a standardised Pandas dataframe containing per-track information and
+    plots distances from the final position for each track, which is a good way
+    to tell how quickly spots are settling down
+    '''
+    drawlines = []
+    tracksgroup = tracks.groupby("track_no")
+    for name, group in tracksgroup:
+        drawlines.append(np.arange(1, len(group["origin_dist"].values)+1)*time)
+        drawlines.append(np.sqrt(group["origin_dist"].values))
+
+    fig = plt.figure(figsize=(10, 3))
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.plot(*drawlines, color='0.6', linewidth=0.5, alpha=0.5)
+    ax1.set_xlabel('time since appearance(s)')
+    ax1.set_ylabel('Distance from final step($nm$)')
+    ax1.set_title("Labelled GFP-TMH minus DTT")
     fig.tight_layout()
     return fig
