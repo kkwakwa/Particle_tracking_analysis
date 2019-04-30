5d0a0545485c7685e441e11411c9f5db02150038
Kwasi A. Kwakwa <kwasi.kwakwa@gmail.com>
2019-04-30 15:01:06 +0100

    Added a function for fitting a double gaussian to data

--- tracking_analysis.py
+++ tracking_analysis.py
@@ -22,3 +22,19 @@ def doublegaussian(x, amp1, amp2, cen1, cen2, wid1, wid2):
 
 def triplegaussian(x, amp1, amp2, amp3, cen1, cen2, cen3, wid1, wid2, wid3):
     return (amp1 * np.exp(-(x-cen1)**2 / wid1)) + (amp2 * np.exp(-(x-cen2)**2 / wid2)) + (amp3 * np.exp(-(x-cen3)**2 / wid3))
+
+
+def gaussian_fit_data(data):
+    '''
+        takes a single column numpy array representing a range of values and
+        then attempts to fit a double gaussian
+
+        Returns an array consisting of [amp1, amp2, cen1, cen2, wid1, wid2]
+
+        standard deviation = sqrt(wid/2)
+    '''
+    hist, edges = np.histogram(data, bins=100, density=True)
+    edges1 = edges[:-1] + np.diff(edges)[0]
+    init_vals = [.005, .005, 50, 250, 10, 10]  # for [amp1,amp2, cen1, cen2, wid1, wid2]
+    best_vals, covar = curve_fit(doublegaussian, edges1, hist, p0=init_vals)
+    return best_vals
