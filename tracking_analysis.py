e8a355b10e965beee1a9261d99c2d9311eec4610
Kwasi A. Kwakwa <kwasi.kwakwa@gmail.com>
2019-04-30 14:10:54 +0100

    Tidying up the tracking analysis functions

--- tracking_analysis.py
+++ tracking_analysis.py
@@ -10,8 +10,6 @@ tracking data analysis
 
 import numpy as np
 from scipy.optimize import curve_fit
-from scipy.stats import linregress
-
 
 
 def gaussian(x, amp, cen, wid):
@@ -24,10 +22,3 @@ def doublegaussian(x, amp1, amp2, cen1, cen2, wid1, wid2):
 
 def triplegaussian(x, amp1, amp2, amp3, cen1, cen2, cen3, wid1, wid2, wid3):
     return (amp1 * np.exp(-(x-cen1)**2 / wid1)) + (amp2 * np.exp(-(x-cen2)**2 / wid2)) + (amp3 * np.exp(-(x-cen3)**2 / wid3))
-
-
-def MSD_fit(values, time=0.03):
-    val_length = len(values//2)
-    slope, intercept, r_value, p_value, std_err = linregress(np.log(np.arange(1, val_length+1)*0.3), np.log(values[:val_length] * 1e-6))
-    return slope
-
