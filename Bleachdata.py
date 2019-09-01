058f095ff92823184599fa94e681c1db80cfdd49
Kwasi A. Kwakwa <kwasi.kwakwa@gmail.com>
2019-09-01 21:36:01 +0100

    Made some changes to bleach import

--- Bleachdata.py
+++ Bleachdata.py
@@ -14,13 +14,24 @@ from matplotlib import pyplot as plt
 from skimage import io
 from skimage.feature import peak_local_max
 from skimage.filters import threshold_otsu
-from skimage.filters import gaussian
+from skimage.filters import median
+
+from glob import glob
+
+directory = 'D:/Data/Fluorescence/Roxie/190606/'
+search = '**/*ome.tif'
+#search = '*ts.csv'
+
+#search = '*all_ts.csv'
+
+filelist = glob(directory+search)
+print(filelist)
 
 
 def steps_extract(filename):
     bleach_stack = io.imread(filename)
-    max_intensity = bleach_stack[:50,:,:].max(0)
-    max_intensity_1 = gaussian(max_intensity, sigma=1.)
+    max_intensity = bleach_stack[:1000].max(0)
+    max_intensity_1 = median(max_intensity)
     thresh = threshold_otsu(max_intensity_1)
     peaks = peak_local_max(max_intensity_1, min_distance=4, threshold_abs=thresh+0.0001)
     traces = []
@@ -39,3 +50,7 @@ def steps_extract(filename):
     #peaklist1 = peaklist.subtract(background, axis=0)
     outfile = filename[:-4]+'-out.csv'
     peaklist.to_csv(path_or_buf=outfile)
+
+for file in filelist:
+    print(file)
+    steps_extract(file)
